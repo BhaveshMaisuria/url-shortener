98b61d5aa8fcd5904ce55c76985fdca7f8336da8
Bhavesh Maisuriya <bhavesh9562@gmail.com>
2026-06-29 15:46:05 +0530

    Fix ESLint errors

--- test/check.ts
+++
@@ -1,9 +0,0 @@
-import request from 'supertest';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
-
-async function test(app: NestFastifyApplication) {
-  const req = await request(app.getHttpServer()).get('/test').expect(302);
-}
